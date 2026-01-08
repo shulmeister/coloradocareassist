3e1e27caa8ae3f5888da32d888f04badc64f3d98
Jason Shulmeister <jason@coloradocareassist.com>
2026-01-08 16:51:05 -0700

    Add Brevo SDK script to head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,22 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400;600;700&family=Montserrat:wght@500;600;700&display=swap" 
           rel="stylesheet" 
         />
+        <script src="https://cdn.brevo.com/js/sdk-loader.js" async></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              // Version: 2.0
+              window.Brevo = window.Brevo || [];
+              Brevo.push([
+                  "init",
+                  {
+                  client_key: "zohlf9pja1k6du3epy2j415y",
+                  // Optional: Add other initialization options, see documentation
+                  }
+              ]);
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
